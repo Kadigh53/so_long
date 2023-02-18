1c201a01958177290e2cb7baef285c207f82714f
k16aO2digOOh <abdlkadraoutem@gmail.com>
2023-02-18 21:02:18 +0100

    ..

--- backtrack.c
+++ backtrack.c
@@ -6,7 +6,7 @@
 /*   By: aaoutem- <aaoutem-@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 16:22:17 by aaoutem-          #+#    #+#             */
-/*   Updated: 2023/02/18 20:39:53 by aaoutem-         ###   ########.fr       */
+/*   Updated: 2023/02/18 21:02:05 by aaoutem-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,9 @@ char	**map_instr(char *av[])
 
 void	flood_fill(char   ***map, int y, int x)
 {
-	if ((*map)[y][x] == '1' || (*map)[y][x] == 'F' || !(*map)[y] || !(*map)[y][x])
+	if ((*map)[y][x] == '1' || (*map)[y][x] == 'X')
 		return ;
-	(*map)[y][x] = 'F';
+	(*map)[y][x] = 'X';
 	flood_fill(map, y, x + 1);
 	flood_fill(map, y, x - 1);
 	flood_fill(map, y + 1, x);
@@ -58,7 +58,10 @@ void    backtrack(char  **av, t_data **data)
 		while(map[i][j])
 		{
 			if (map[i][j] == 'C' || map[i][j] == 'E')
+			{
+				//free(map);	
 				return (NULL);
+			}
 			j++;
 		}
 		i++;
@@ -74,6 +77,7 @@ void    backtrack(char  **av, t_data **data)
 // 	map = map_instr(av);
 // 	flood_fill(&map, 4, 13);
 // 	printf("{{%s}\n {%s}\n {%s}\n {%s}\n {%s}\n {%s}}\n\n",(map)[0],(map)[1],(map)[2],(map)[3],(map)[4],(map)[5]);
+// }
 // 	int i,j;
 // 	i =0;
 // 	while (map[i])
